4964a56319ca205b369961e7925f179176ca150c
Eric Wong <ericwong12@live.com>
2020-09-19 15:24:10 -0600

    Updated error page

--- src/views/User/404.vue
+++ src/views/User/404.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="container-fluid">
-    <h1>Seems like the page you are looking for doesnt exist.</h1>
-    <h2>
-      Back to
-      <router-link to="/">home!</router-link>
-    </h2>
+    <div class="errorBox">
+      <div class="errorText">
+        <!-- <div class="error"> -->
+        <h1>Seems like the page you are looking for doesnt exist.</h1>
+        <h2>
+          Back to
+          <router-link to="/">home!</router-link>
+        </h2>
+        <!-- </div> -->
+      </div>
+
+      <img class="errorImage" src="/img/UI/dog.png" alt />
+    </div>
   </div>
 </template>
 
@@ -18,12 +26,32 @@ export default {};
   height: 100vh;
   text-align: center;
   min-width: 100%;
-  margin: 0 !important;
-  background: url("/img/UI/error.png") no-repeat center center fixed;
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: 100% auto;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-@media screen and (max-width: 750px;);
+.errorBox {
+  /* display: grid; */
+  height: 50vh;
+  grid-template-columns: [line1]50% [line2]50%;
+  /* grid-template-rows: [row1-start]50% [row1-end]50%; */
+  justify-content: center;
+  align-content: center;
+}
+/* .errorText {
+  font-size: 3rem !important;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+} */
+.errorImage {
+  max-width: 100%;
+  max-height: 100%;
+  /* grid-column-start: line1;
+  grid-column-end: line2; */
+}
+@media screen and (max-width: 750px) {
+}
 </style>
