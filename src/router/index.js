1a6c553807946dc5790aca44304f222d0f3874e6
Eric Wong <ericwong12@live.com>
2020-09-22 20:38:10 -0600

    Some tinkering but still broken

--- src/router/index.js
+++ src/router/index.js
@@ -1,103 +1,88 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import VueCompositionApi from "@vue/composition-api"
+import Vue from "vue";
+import VueRouter from "vue-router";
+import VueCompositionApi from "@vue/composition-api";
 //Routes
-import About from "../views/User/About.vue"
-import Admin from '../views/Admin/Admin.vue'
-import Dashboard from "../views/User/Dashboard.vue"
-import EditPost from "../views/Admin/EditPost.vue"
-import NewPost from "../views/Admin/NewPost.vue"
-import ViewPost from "../views/User/ViewPost.vue"
-import Login from "../views/User/Login.vue"
-import Error from "../views/User/404.vue"
+import About from "../views/User/About.vue";
+import Admin from "../views/Admin/Admin.vue";
+import Dashboard from "../views/User/Dashboard.vue";
+import EditPost from "../views/Admin/EditPost.vue";
+import NewPost from "../views/Admin/NewPost.vue";
+import ViewPost from "../views/User/ViewPost.vue";
+import Login from "../views/User/Login.vue";
+import Error from "../views/User/404.vue";
 // Store
-import useAuth from "../store/modules/auth"
-import {
-  watchEffect,
-  computed
-} from "@vue/composition-api"
-// import {
-//   reactive
-// } from "@vue/composition-api"
-Vue.use(VueCompositionApi)
-const {
-  authenticated,
-} = useAuth()
-console.log(authenticated)
-watchEffect(() => {
-  authenticated
-})
+import useAuth from "../store/modules/auth";
+import { reactive } from "@vue/composition-api";
+Vue.use(VueCompositionApi);
+Vue.use(VueRouter);
 
-Vue.use(VueRouter)
-const routes = [{
-    path: '/',
-    name: 'dashboard',
-    component: Dashboard
+let auth = reactive(useAuth().authenticated);
+console.log(auth);
+const routes = [
+  {
+    path: "/",
+    name: "dashboard",
+    component: Dashboard,
   },
   {
     path: "*",
     name: "error",
-    component: Error
+    component: Error,
   },
-  { //Added :post_id so you can edit the specific post
-    path: '/admin/edit-post/:title',
-    name: 'edit-post',
+  {
+    //Added :post_id so you can edit the specific post
+    path: "/admin/edit-post/:title",
+    name: "edit-post",
     component: EditPost,
     meta: {
-      requiresAuth: true
-    }
+      requiresAuth: true,
+    },
   },
   {
-    path: '/admin/new-post',
-    name: 'new-post',
+    path: "/admin/new-post",
+    name: "new-post",
     component: NewPost,
     meta: {
-      requiresAuth: true
-    }
+      requiresAuth: true,
+    },
   },
   {
-    path: '/view-post/:title',
-    name: 'view-post',
-    component: ViewPost
+    path: "/view-post/:title",
+    name: "view-post",
+    component: ViewPost,
   },
   {
-    path: '/about',
-    name: 'About',
-    component: About
+    path: "/about",
+    name: "About",
+    component: About,
   },
   {
     path: "/login",
     name: "login",
-    component: Login
+    component: Login,
   },
   {
-    path: '/admin',
-    name: 'admin',
+    path: "/admin",
+    name: "admin",
     component: Admin,
     meta: {
-      requiresAuth: true
-    }
+      requiresAuth: true,
+    },
   },
-]
-
+];
+// console.log(useAuth().authenticated);
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
 router.beforeEach((to, from, next) => {
-  if (to.matched.some(record => record.meta.requiresAuth)) {
-    if (!authenticated) {
-      next({
-        path: "/login"
-      });
-    } else {
-      next()
-    }
+  if (to.matched.some((record) => record.meta.requiresAuth) && !auth) {
+    next("/login");
   } else {
-    next()
+    next();
   }
-})
+});
 
-export default router
+export default router;
--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,49 +1,60 @@
-import {
-    reactive,
-    toRefs,
-} from "@vue/composition-api";
-import {
-    auth
-} from "../../components/firebase/firebaseInit"
-import router from "@/router"
+import { reactive, toRefs } from "@vue/composition-api";
+import { auth } from "../../components/firebase/firebaseInit";
+import router from "@/router";
 
 export default function useAuth() {
-    const authState = reactive({
-        authenticated: false,
-        error: null
-    })
-    const login = (email, password) => {
-        auth.signInWithEmailAndPassword(email, password).then(() => {
-            router.push("/admin")
-        }).catch((error) => {
-            authState.error = error
-            console.log(error)
-        })
-        // authState.authenticated = true
-        // console.log(authState.authenticated)
-    }
-    const logout = () => {
-        auth.signOut().then(() => {
-            router.push("/")
-        }).catch((error) => {
-            state.error = error
-            console.log(error)
-        })
-    }
-    const status = () => {
-        auth.onAuthStateChanged(user => {
-            if (user) {
-                authState.authenticated = true
-            } else {
-                authState.authenticated = false
-            }
-        })
-    }
-    return {
-        ...toRefs(authState),
-        authState,
-        login,
-        logout,
-        status
-    }
+  const authState = reactive({
+    authenticated: false,
+    error: null,
+  });
+  const login = (email, password) => {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        auth.onAuthStateChanged((user) => {
+          if (user) {
+            authState.authenticated = true;
+          }
+        });
+        router.push("/admin");
+      })
+      .catch((error) => {
+        authState.error = error;
+        console.log(error);
+      });
+    // authState.authenticated = true
+    // console.log(authState.authenticated)
+  };
+  const logout = () => {
+    auth
+      .signOut()
+      .then(() => {
+        auth.onAuthStateChanged((user) => {
+          if (user) {
+            authState.authenticated = false;
+          }
+        });
+        router.push("/");
+      })
+      .catch((error) => {
+        state.error = error;
+        console.log(error);
+      });
+  };
+  const status = () => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        authState.authenticated = true;
+      } else {
+        authState.authenticated = false;
+      }
+    });
+  };
+  return {
+    ...toRefs(authState),
+    authState,
+    login,
+    logout,
+    status,
+  };
 }
