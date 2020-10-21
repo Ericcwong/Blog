d77fb1cf6174a065c0ad657a04a502ab74aa143a
Eric Wong <ericwong12@live.com>
2020-10-20 20:18:01 -0600

    added static configuration

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,17 @@
   <div id="app">
     <Navbar />
     <router-view />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Navbar from "./components/UI/NavBar";
+import Footer from "./components/UI/Footer"
 export default {
   components: {
     Navbar,
+    Footer
   },
 };
 </script>
