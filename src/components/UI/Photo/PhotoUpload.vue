288a905ea5e83c0e135ada894ba772293942cdc1
Eric Wong <ericwong12@live.com>
2020-10-27 20:42:36 -0600

    Photo upload component Destructuring each file upload and needs end point to upload

--- src/components/UI/Photo/PhotoUpload.vue
+++ src/components/UI/Photo/PhotoUpload.vue
@@ -7,9 +7,20 @@
 
 <script>
 export default {
+  data() {
+    return {
+      files: [],
+    };
+  },
   methods: {
     uploadImages(e) {
       let fileSelected = e.target.files[0];
+      let filesSelected = e.target.files;
+      for (let i = 0; i < filesSelected.length; i++) {
+        console.log(e.target.files[i]);
+        // this.files.push();
+      }
+      console.log(filesSelected);
       console.log(e);
     },
   },
