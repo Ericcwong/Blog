56b87a7f9e44552b936b3107da67566712252074
Eric Wong <ericwong12@live.com>
2020-11-04 19:32:36 -0700

    heroku deployment

---
+++ dist/precache-manifest.f1927269cc0587ceed8d4e52588dae14.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bac04831c1090e2be242",
+    "url": "/css/app.a84688d6.css"
+  },
+  {
+    "revision": "5ad28d1a515d8094c576",
+    "url": "/css/chunk-vendors.8c1106a2.css"
+  },
+  {
+    "revision": "d4158674a4bc9b695aa05b00e20ff9aa",
+    "url": "/img/UI/dog.png"
+  },
+  {
+    "revision": "af8f37f9b717189f49a8a0f93fdff358",
+    "url": "/img/UI/waveBackground.mp4"
+  },
+  {
+    "revision": "15a8cabf4478458f33d366c2d68e884d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bac04831c1090e2be242",
+    "url": "/js/app.284a4c85.js"
+  },
+  {
+    "revision": "5ad28d1a515d8094c576",
+    "url": "/js/chunk-vendors.62b1693c.js"
+  },
+  {
+    "revision": "69b649f3f7f4f8f6e1b3afed797a8459",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "f2d69af7cf3639e4c0773c091f9b8f53",
+    "url": "/readMeImg/code.png"
+  },
+  {
+    "revision": "78656bd3f51c3583c35d588aa3a05721",
+    "url": "/readMeImg/codeCopy.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
