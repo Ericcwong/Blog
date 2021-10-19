7a2960a8836181c7323fa977e96da942815ba17a
Eric Wong <ericwong12@live.com>
2021-10-19 16:25:25 -0600

    Fix: Navbar
    Navigation to blog is taking out, that page was not used.
    
    Home page would handle displaying the blogs.

--- src/components/UI/NavBar.vue
+++ src/components/UI/NavBar.vue
@@ -9,7 +9,7 @@
       <b-navbar-nav class="ml-auto">
         <b-navbar-nav>
           <b-nav-item to="/">Home</b-nav-item>
-          <b-nav-item to="/blogs">Blogs</b-nav-item>
+          <!-- <b-nav-item to="/blogs">Blogs</b-nav-item> -->
           <b-nav-item v-if="userStatus" to="/admin/new-post"
             >New Post</b-nav-item
           >
